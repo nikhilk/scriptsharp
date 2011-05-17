2b8ece89fe64265d90e4ae63ebfcf30b1509cc39
nikhilk <nikhilk@live.com>
2011-05-17 09:43:00 -0700

    Script# FxCop rules - sources for ScriptSharp.FxCop assembly

---
+++ src/Tools/FxCop/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿// AssemblyInfo.cs
+// Script#/Tools/FxCop
+// Copyright (c) Nikhil Kothari, 2008. All Rights Reserved.
+//
+// Assembly Metadata
+//
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ScriptSharp.FxCop")]
+[assembly: AssemblyDescription("Script# FxCop Rules")]
---
+++ src/Tools/FxCop/ScriptUnusedFields.cs
@@ -0,0 +1,62 @@
+﻿// ScriptInvalidSelector.cs
+// Script#/Tools/FxCop
+// Copyright (c) Nikhil Kothari.
+// Copyright (c) Microsoft Corporation.
+// This source code is subject to terms and conditions of the Microsoft 
+// Public License. A copy of the license can be found in License.txt.
+//
+
+using System;
+using System.Diagnostics;
+using Microsoft.FxCop.Sdk;
+
+namespace ScriptSharp.FxCop {
+
+    public sealed class ScriptUnusedFields : BaseIntrospectionRule {
+
+        private TypeNode _importedAttributeType;
+        private TypeNode _recordType;
+
+        public ScriptUnusedFields() :
+            base(typeof(ScriptUnusedFields).Name,
+                 typeof(ScriptUnusedFields).Namespace + ".RuleData",
+                 typeof(ScriptUnusedFields).Assembly) {
+        }
+
+        public override TargetVisibilities TargetVisibility {
+            get {
+                return (TargetVisibilities.Overridable | TargetVisibilities.ExternallyVisible);
+            }
+        }
+
+        public override void BeforeAnalysis() {
+            base.BeforeAnalysis();
+
+            _importedAttributeType =
+                FrameworkAssemblies.Mscorlib.GetType(Identifier.For("System.Runtime.CompilerServices"),
+                                                     Identifier.For("ImportedAttribute"));
+            _recordType =
+                FrameworkAssemblies.Mscorlib.GetType(Identifier.For("System"),
+                                                     Identifier.For("Record"));
+        }
+
+        public override ProblemCollection Check(Member member) {
+            if (!(member is Field) || member.IsStatic || member.IsPrivate) {
+                return null;
+            }
+
+            // If the declaring type has [Imported] applied, we have no choice but to allow the field to exist
+            if (RuleUtilities.HasCustomAttribute(member.DeclaringType, _importedAttributeType)) {
+                return null;
+            }
+
+            // If the type derives from Record, allow the field to exist
+            if (member.DeclaringType.BaseType == _recordType) {
+                return null;
+            }
+
+            Problems.Add(new Problem(base.GetResolution(new object[] { member })));
+            return Problems;
+        }
+    }
+}
