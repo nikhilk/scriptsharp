45e9ca8c4b1c644cc4417ed806ea8fc6f26854d9
nikhilk <nikhilk@live.com>
2011-06-01 18:30:20 +0800

    Sharpen.Global.js script - CultureInfo along with richer Number/Date formatting capability as optional functionality.

---
+++ src/Runtime/Client/Core/Global.js
@@ -0,0 +1,11 @@
+//! Sharpen Core Runtime
+//! Globalization Functionality
+//!
+
+(function() {
+
+#include "Global\Culture.js"
+#include "Global\Number.js"
+#include "Global\Date.js"
+
+})();
---
+++ src/Runtime/Client/Core/Global/Culture.js
@@ -0,0 +1,71 @@
+// CultureInfo definitions
+
+ss.neutralCulture = {
+  name: '',
+  // numberFormat
+  nf: {
+      nan: 'NaN',           // naNSymbol
+      neg: '-',             // negativeSign
+      pos: '+',             // positiveSign
+      negInf: '-Infinity',  // negativeInfinityText
+      posInf: 'Infinity',   // positiveInfinityText
+      gw: [3],              // numberGroupSizes
+      dd: 2,                // numberDecimalDigits
+      ds: '.',              // numberDecimalSeparator
+      gs: ',' ,             // numberGroupSeparator
+
+      per: '%',             // percentSymbol
+      perGW: [3],           // percentGroupSizes
+      perDD: 2,             // percentDecimalDigits
+      perDS: '.',           // percentDecimalSeparator
+      perGS: ',',           // percentGroupSeparator
+      perPP: '{0} %',       // percentPositivePattern
+      perNP: '-{0} %',      // percentNegativePattern
+
+      cur: '$',             // currencySymbol
+      curGW: [3],           // currencyGroupSizes
+      curDD: 2,             // currencyDecimalDigits
+      curDS: '.',           // currencyDecimalSeparator
+      curGS: ',',           // currencyGroupSeparator
+      curNP: '(${0})',      // currencyNegativePattern
+      curPP: '${0}'         // currencyPositivePattern
+    },
+    // dateFormat
+    dtf: {
+      am: 'AM',             // amDesignator
+      pm: 'PM',             // pmDesignator
+
+      ds: '/',              // dateSeparator
+      ts: ':',              // timeSeparator
+
+      gmt: 'ddd, dd MMM yyyy HH:mm:ss \'GMT\'',   // gmtDateTimePattern
+      uni: 'yyyy-MM-dd HH:mm:ssZ',                // universalDateTimePattern
+      sort: 'yyyy-MM-ddTHH:mm:ss',                // sortableDateTimePattern
+      dt: 'dddd, MMMM dd, yyyy h:mm:ss tt',       // dateTimePattern
+
+      ld: 'dddd, MMMM dd, yyyy',                  // longDatePattern
+      sd: 'M/d/yyyy',                             // shortDatePattern
+
+      lt: 'h:mm:ss tt',                           // longTimePattern
+      st: 'h:mm tt',                              // shortTimePattern
+
+      day0: 0,                                    // firstDayOfWeek
+      day: ['Sunday', 'Monday', 'Tuesday',
+            'Wednesday', 'Thursday',
+            'Friday', 'Saturday'],                // dayNames
+      sday: ['Sun', 'Mon', 'Tue', 'Wed',
+             'Thu', 'Fri', 'Sat'],                // shortDayNames
+      mday: ['Su', 'Mo', 'Tu', 'We',
+             'Th', 'Fr', 'Sa'],                   // minimizedDayNames
+
+      mon: ['January', 'February', 'March',
+            'April', 'May', 'June', 'July',
+            'August', 'September', 'October',
+            'November', 'December', ''],          // monthNames
+      smon: ['Jan', 'Feb', 'Mar', 'Apr',
+             'May', 'Jun', 'Jul', 'Aug',
+             'Sep', 'Oct', 'Nov', 'Dec', '']      // shortMonthNames
+    }
+};
+
+(ss.currentCulture = ss.extend({}, ss.neutralCulture)).name = 'en-us';
