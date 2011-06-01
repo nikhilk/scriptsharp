90f1614bf5d7ae93d8f5223eef0c0ecacafba4d0
Robert Byrne <rob@kapitar.com>
2011-06-01 23:13:49 +0800

    Changed BindingHandler to an abstract alias for Object so it can be implemented in client code.

--- src/Libraries/Knockout/BindingHandler.cs
+++ src/Libraries/Knockout/BindingHandler.cs
@@ -9,7 +9,8 @@ namespace KnockoutApi {
     /// </summary>
     [Imported]
     [IgnoreNamespace]
-    public interface IBindingHandler {
+    [ScriptName("Object")]
+    public abstract class BindingHandler {
         /// <summary>
         /// Performs one time initialization for a binding.
         /// </summary>
@@ -17,7 +18,8 @@ namespace KnockoutApi {
         /// <param name="valueAccessor">A function which returns the model property that is involved in this binding.</param>
         /// <param name="allBindingsAccessor">A function which returns all the model properties bound to this element.</param>
         /// <param name="viewModel">The view model instance involved in this binding.</param>
-        void Init(Element element, Func<object> valueAccessor, Func<Dictionary> allBindingsAccessor, object viewModel);
+        public virtual void Init(Element element, Func<object> valueAccessor, Func<Dictionary> allBindingsAccessor, object viewModel) {
+        }
 
         /// <summary>
         /// Invoked whenever an observable associated with this binding changes.
@@ -26,6 +28,7 @@ namespace KnockoutApi {
         /// <param name="valueAccessor">A function which returns the model property that is involved in this binding.</param>
         /// <param name="allBindingsAccessor">A function which returns all the model properties bound to this element.</param>
         /// <param name="viewModel">The view model instance involved in this binding.</param>
-        void Update(Element element, Func<object> valueAccessor, Func<Dictionary> allBindingsAccessor, object viewModel);
+        public virtual void Update(Element element, Func<object> valueAccessor, Func<Dictionary> allBindingsAccessor, object viewModel) {
+        }
     }
 }
