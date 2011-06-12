781cbf7cbd79ac11f6947f6c48d044d70754c892
nikhilk <nikhilk@live.com>
2011-06-13 06:18:58 +0800

    Fix code formatting/style for consistency

--- src/Libraries/Web/Html/ClientRect.cs
+++ src/Libraries/Web/Html/ClientRect.cs
@@ -1,4 +1,9 @@
 // ClientRect.cs
+// Script#/Libraries/Web
+// Copyright (c) Nikhil Kothari.
+// Copyright (c) Microsoft Corporation.
+// This source code is subject to terms and conditions of the Microsoft 
+// Public License. A copy of the license can be found in License.txt.
 //
 
 using System;
