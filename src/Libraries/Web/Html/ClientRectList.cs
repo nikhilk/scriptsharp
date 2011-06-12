bb706b0f7312dae85ec56c42a5aebd2974cb7ee8
nikhilk <nikhilk@live.com>
2011-06-13 06:10:58 +0800

    Fix code formatting/style and metadata definitions

--- src/Libraries/Web/Html/ClientRectList.cs
+++ src/Libraries/Web/Html/ClientRectList.cs
@@ -6,39 +6,26 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace System.Html {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <remarks>
-    /// see http://www.w3.org/TR/cssom-view/#clientrectlist
-    /// interface ClientRectList {
-    ///   readonly attribute unsigned long length;
-    ///   getter ClientRect item(unsigned long index);
-    /// };
-    /// </remarks>
+
     [IgnoreNamespace]
     [Imported]
     public sealed class ClientRectList {
-        private ClientRectList() {
 
+        private ClientRectList() {
         }
 
         [IntrinsicProperty]
         public int Length {
-            get { return 0; }
+            get {
+                return 0;
+            }
         }
 
-		[IntrinsicProperty]
-    	public ClientRect this[ulong index]
-    	{
-			get
-			{
-				return null;
-			}
-    	}
-
-        public IEnumerator<ClientRect> GetEnumerator() {
-            return null;
+        [IntrinsicProperty]
+        public ClientRect this[int index] {
+            get {
+                return null;
+            }
         }
     }
 }
