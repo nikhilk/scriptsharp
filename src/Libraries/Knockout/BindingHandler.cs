17551f251d79a889dca5890460147bc917072c46
nikhilk <nikhilk@live.com>
2011-06-18 07:30:20 +0800

    Misc code cleanup.

--- src/Libraries/Knockout/BindingHandler.cs
+++ src/Libraries/Knockout/BindingHandler.cs
@@ -1,9 +1,16 @@
+// BindingHandler.cs
+// Script#/Libraries/Knockout
+// This source code is subject to terms and conditions of the Microsoft 
+// Public License. A copy of the license can be found in License.txt.
+//
+
 using System;
 using System.Collections;
 using System.Html;
 using System.Runtime.CompilerServices;
 
 namespace KnockoutApi {
+
     /// <summary>
     /// Represents a custom binding handler in Knockout.
     /// </summary>
@@ -11,6 +18,7 @@ namespace KnockoutApi {
     [IgnoreNamespace]
     [ScriptName("Object")]
     public abstract class BindingHandler {
+
         /// <summary>
         /// Performs one time initialization for a binding.
         /// </summary>
--- src/Libraries/Knockout/DependentObservableOptions.cs
+++ src/Libraries/Knockout/DependentObservableOptions.cs
@@ -1,7 +1,14 @@
+// DependentObservableOptions.cs
+// Script#/Libraries/Knockout
+// This source code is subject to terms and conditions of the Microsoft 
+// Public License. A copy of the license can be found in License.txt.
+//
+
 using System;
 using System.Runtime.CompilerServices;
 
 namespace KnockoutApi {
+
     /// <summary>
     /// Provides advanced options for defining a dependent observable.
     /// </summary>
@@ -9,25 +16,36 @@ namespace KnockoutApi {
     [Imported]
     [IgnoreNamespace]
     [ScriptName("Object")]
-    public class DependentObservableOptions<T> {
+    public sealed class DependentObservableOptions<T> {
+
         /// <summary>
-        /// A function to compute the value.
+        /// Gets or sets whether the evaluation should be deferred, i.e. not
+        /// performed when the observable is first created.
+        /// </summary>
+        [IntrinsicProperty]
+        public bool DeferEvaluation {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the function to compute the value.
         /// </summary>
         [ScriptName("read")]
         [IntrinsicProperty]
-        public Func<T> GetValueFunction { get; set; }
+        public Func<T> GetValueFunction {
+            get;
+            set;
+        }
 
         /// <summary>
-        /// The model instance which acts as 'this' in the get value function.
+        /// Gets the model instance which acts as 'this' in the get value function.
         /// </summary>
         [ScriptName("owner")]
         [IntrinsicProperty]
-        public object Model { get; set; }
-
-        /// <summary>
-        /// If true, evaluation is not performed when the observable is first created, which can be useful if it relies on state which may not be ready.
-        /// </summary>
-        [IntrinsicProperty]
-        public bool DeferEvaluation { get; set; }
+        public object Model {
+            get;
+            set;
+        }
     }
 }
