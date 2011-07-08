db4a7b7203a1b0adb095ff6e29c8faeb70fe1546
Matt Clay <matt.clay@earthclassmail.com>
2011-07-09 01:42:42 +0800

    Added HTML5 properties and methods to History.

--- src/Libraries/Web/Html/History.cs
+++ src/Libraries/Web/Html/History.cs
@@ -46,5 +46,49 @@ namespace System.Html {
         /// <param name="delta">The number of pages in history to go back (negative) or forward (positive).</param>
         public void Go(int delta) {
         }
+
+        /// <summary>
+        /// Retrieves the current state object.
+        /// </summary>
+        [IntrinsicProperty]
+        public object State {
+            get {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Pushes the given data onto the session history stack.
+        /// </summary>
+        /// <param name="data">The data to serialize into JSON format.</param>
+        /// <param name="title">The title to place into history.</param>
+        [AlternateSignature]
+        public extern void PushState(object data, string title);
+
+        /// <summary>
+        /// Pushes the given data onto the session history stack.
+        /// </summary>
+        /// <param name="data">The data to serialize into JSON format.</param>
+        /// <param name="title">The title to place into history.</param>
+        /// <param name="url">The URL to place into history.</param>
+        public void PushState(object data, string title, string url) {
+        }
+
+        /// <summary>
+        /// Updates the most recent entry on the history stack.
+        /// </summary>
+        /// <param name="data">The data to serialize into JSON format.</param>
+        /// <param name="title">The title to place into history.</param>
+        [AlternateSignature]
+        public extern void ReplaceState(object data, string title);
+
+        /// <summary>
+        /// Updates the most recent entry on the history stack.
+        /// </summary>
+        /// <param name="data">The data to serialize into JSON format.</param>
+        /// <param name="title">The title to place into history.</param>
+        /// <param name="url">The URL to place into history.</param>
+        public void ReplaceState(object data, string title, string url) {
+        }
     }
 }
