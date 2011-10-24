e8e12f058e7668e65b3f628d588233a77b7205a4
nikhilk <nikhilk@live.com>
2011-10-23 21:13:53 -0700

    Misc updates

--- samples/AroundMe/AroundMeWeb/Content/Scripts/mscorlib.debug.js
+++ samples/AroundMe/AroundMeWeb/Content/Scripts/mscorlib.debug.js
@@ -7,7 +7,7 @@
 
 (function () {
   var globals = {
-    version: '0.7.3.0',
+    version: '0.7.4.0',
 
     isUndefined: function (o) {
       return (o === undefined);
@@ -1140,8 +1140,6 @@ Type.prototype.registerEnum = function Type$createEnum(name, flags) {
     if (flags) {
         this.__flags = true;
     }
-    
-    this.toString = ss.Enum._enumToString;
 }
 
 Type.prototype.setupBase = function Type$setupBase() {
@@ -1321,78 +1319,6 @@ Type.getType = function Type$getType(typeName) {
 
 Type.parse = function Type$parse(typeName) {
     return Type.getType(typeName);
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Enum
-
-ss.Enum = function Enum$() {
-}
-ss.Enum.registerClass('Enum');
-
-ss.Enum.parse = function Enum$parse(enumType, s) {
-    var values = enumType.prototype;
-    if (!enumType.__flags) {
-        for (var f in values) {
-            if (f === s) {
-                return values[f];
-            }
-        }
-    }
-    else {
-        var parts = s.split('|');
-        var value = 0;
-        var parsed = true;
-
-        for (var i = parts.length - 1; i >= 0; i--) {
-            var part = parts[i].trim();
-            var found = false;
-
-            for (var f in values) {
-                if (f === part) {
-                    value |= values[f];
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                parsed = false;
-                break;
-            }
-        }
-
-        if (parsed) {
-            return value;
-        }
-    }
-    throw 'Invalid Enumeration Value';
-}
-
-ss.Enum._enumToString = function Enum$toString(value) {
-    var values = this.prototype;
-    if (!this.__flags || (value === 0)) {
-        for (var i in values) {
-            if (values[i] === value) {
-                return i;
-            }
-        }
-        throw 'Invalid Enumeration Value';
-    }
-    else {
-        var parts = [];
-        for (var i in values) {
-            if (values[i] & value) {
-                if (parts.length) {
-                    parts.add(' | ');
-                }
-                parts.add(i);
-            }
-        }
-        if (!parts.length) {
-            throw 'Invalid Enumeration Value';
-        }
-        return parts.join('');
-    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
