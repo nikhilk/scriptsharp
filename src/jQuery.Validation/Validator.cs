b98f010bd740c2282b260b417e97940a4c2e41be
elkdanger <elkdanger@googlemail.com>
2012-01-13 14:07:03 +0000

    Added instance methods to Validator

--- src/jQuery.Validation/Validator.cs
+++ src/jQuery.Validation/Validator.cs
@@ -4,6 +4,8 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Html;
+using System.Collections;
 
 namespace jQueryApi.Validation
 {
@@ -11,7 +13,47 @@ namespace jQueryApi.Validation
 	[IgnoreNamespace]
 	public class Validator	{
 
+		/// <summary>
+		/// Validates the specified element
+		/// </summary>
+		/// <param name="element">The element to validate</param>
+		/// <returns>True if the form is valid, otherwise returns false.</returns>
+		[ScriptName("element")]
+		public bool ValidateElement(Element element) {
+			return false;
+		}
 
+		/// <summary>
+		/// Validates the controlled form
+		/// </summary>
+		/// <returns>True if the form is valid, otherwise returns false.</returns>
+		[ScriptName("form")]
+		public bool ValidateForm()	{
+			return false;
+		}
+
+		/// <summary>
+		/// Gets the number of invalid fields on the controlled form.
+		/// </summary>
+		/// <returns>The number of invalid fields</returns>
+		[ScriptName("numberOfInvalids")]
+		public int GetInvalidFieldCount() {
+			return 0;
+		}
+
+		/// <summary>
+		/// Resets the controlled form
+		/// </summary>
+		public void ResetForm() {
+		}
+
+		/// <summary>
+		/// Shows the specified errors messages.
+		/// </summary>
+		/// <param name="errors">Key/value pairs of error messages, where the keys relate to names of elements in the form, and
+		/// the values are the error messages to display for those elements.</param>
+		public void ShowErrors(Dictionary errors) {
+		}
 
 	}
 }
