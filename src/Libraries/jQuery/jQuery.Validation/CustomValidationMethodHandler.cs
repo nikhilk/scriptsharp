a3ec6efb75622d38a9075f5ebe689ede0145ad6e
Nikhil Kothari <nikhilk@live.com>
2012-05-18 18:43:02 -0700

    Style consistency - tabs/formatting
    Move Format method to Validator class instead of being on jQueryValidationObject to match
    actual jQuery validation API

--- src/Libraries/jQuery/jQuery.Validation/CustomValidationMethodHandler.cs
+++ src/Libraries/jQuery/jQuery.Validation/CustomValidationMethodHandler.cs
@@ -1,11 +1,13 @@
 // CustomValidationMethodHandler.cs
+// Script#/Libraries/jQuery/jQueryValidation
+// This source code is subject to terms and conditions of the Microsoft 
+// Public License. A copy of the license can be found in License.txt.
 //
 
 using System;
-using System.Collections.Generic;
 using System.Html;
 
-namespace jQueryApi.Validation
-{
-	public delegate bool CustomValidationMethodHandler(string value, Element element, object [] parameters);
+namespace jQueryApi.Validation {
+
+	public delegate bool CustomValidationMethodHandler(string value, Element element, object[] parameters);
 }
