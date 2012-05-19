c3c363ff5d2dafad0aa51e9bd1ea779c7880ed59
Nikhil Kothari <nikhilk@live.com>
2012-05-18 18:42:07 -0700

    Make validation script library consistent with other script libraries

--- src/Libraries/jQuery/jQuery.Validation/Properties/AssemblyInfo.cs
+++ src/Libraries/jQuery/jQuery.Validation/Properties/AssemblyInfo.cs
@@ -1,22 +1,12 @@
 ﻿// AssemblyInfo.cs
+// Script#/Runtime/jQuery/jQuery.Validation
+// Copyright (c) Nikhil Kothari, 2006
 //
 
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("jQuery.Validation")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("jQuery.Validation")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: ScriptAssembly("jQuery.Validation")]
+[assembly: AssemblyTitle("Script.jQuery.Validation")]
+[assembly: AssemblyDescription("Script# jQuery Validation Plugin")]
+[assembly: ScriptAssembly("jQueryValidation")]
