812a51dfe4dc08630b5c0a5a4513c5f0630434de
Nikhil Kothari <nikhilk@live.com>
2012-05-22 10:52:13 -0700

    Fix setup build script to infer root directory

--- setup/build.bat
+++ setup/build.bat
@@ -1,19 +1,24 @@
 @echo off
 
-if not exist %1\bin\Release\Setup mkdir %1\bin\Release\Setup
-del /q %1\bin\Release\Setup\*.*
+set _=%cd%
+cd %~dp0\..\setup
+
+if not exist ..\bin\Release\Setup mkdir ..\bin\Release\Setup
+del /q ..\bin\Release\Setup\*.*
 
 @echo .
 
 @echo .
 @echo Compiling...
-%1\tools\bin\Wix\candle.exe -nologo -sw1068 -out %1\bin\Release\Setup\ ScriptSharp.wxs
+..\tools\bin\Wix\candle.exe -nologo -sw1068 -out ..\bin\Release\Setup\ ScriptSharp.wxs
 
 @echo .
 @echo Linking...
-%1\tools\bin\Wix\light.exe -nologo -sw1076 %1\bin\Release\Setup\*.wixobj -ext WixNetFxExtension -ext WixUIExtension -b %1\bin\Release -out %1\bin\Release\Setup\ScriptSharp.msi
+..\tools\bin\Wix\light.exe -nologo -sw1076 ..\bin\Release\Setup\*.wixobj -ext WixNetFxExtension -ext WixUIExtension -b ..\bin\Release -out ..\bin\Release\Setup\ScriptSharp.msi
 
 @echo .
 @echo Cleaning up...
-del /q %1\bin\Release\Setup\*.wixobj
-del /q %1\bin\Release\Setup\*.wixpdb
+del /q ..\bin\Release\Setup\*.wixobj
+del /q ..\bin\Release\Setup\*.wixpdb
+
+chdir /d %_%
