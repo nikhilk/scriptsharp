c4352f91e94e3df91d6de0f5149c269aca91f276
Ivaylo Gochkov <igochkov@gmail.com>
2012-06-30 01:00:18 +0300

    Implementation of the mouse toggle event

--- src/Libraries/jQuery/jQuery.Core/jQueryObject.cs
+++ src/Libraries/jQuery/jQuery.Core/jQueryObject.cs
@@ -3433,6 +3433,17 @@ namespace jQueryApi {
         }
 
         /// <summary>
+        /// Bind two or more handlers to the matched elements, to be executed on alternate clicks.
+        /// </summary>
+        /// <param name="evenEventHandler">A function to execute every even time the element is clicked.</param>
+        /// <param name="oddEventHandler">A function to execute every odd time the element is clicked.</param>
+        /// <param name="eventHandlers">Additional handlers to cycle through after clicks.</param>
+        /// <returns>The current jQueryObject.</returns>
+        public jQueryObject Toggle(jQueryEventHandler evenEventHandler, jQueryEventHandler oddEventHandler, params jQueryEventHandler[] eventHandlers) {
+            return null;
+        }
+
+        /// <summary>
         /// Toggles the specified class from each of the set of matched elements.
         /// </summary>
         /// <param name="className">The class to toggle.</param>
