c66db2bf225d0d8aa3dee302a73d40f1c093238d
Ivaylo Gochkov <igochkov@gmail.com>
2012-06-30 11:47:49 +0300

    Added an overload for just two handlers

--- src/Libraries/jQuery/jQuery.Core/jQueryObject.cs
+++ src/Libraries/jQuery/jQuery.Core/jQueryObject.cs
@@ -3437,6 +3437,16 @@ namespace jQueryApi {
         /// </summary>
         /// <param name="evenEventHandler">A function to execute every even time the element is clicked.</param>
         /// <param name="oddEventHandler">A function to execute every odd time the element is clicked.</param>
+        /// <returns>The current jQueryObject.</returns>
+        public jQueryObject Toggle(jQueryEventHandler evenEventHandler, jQueryEventHandler oddEventHandler) {
+            return null;
+        }
+
+        /// <summary>
+        /// Bind two or more handlers to the matched elements, to be executed on alternate clicks.
+        /// </summary>
+        /// <param name="evenEventHandler">A function to execute every even time the element is clicked.</param>
+        /// <param name="oddEventHandler">A function to execute every odd time the element is clicked.</param>
         /// <param name="eventHandlers">Additional handlers to cycle through after clicks.</param>
         /// <returns>The current jQueryObject.</returns>
         public jQueryObject Toggle(jQueryEventHandler evenEventHandler, jQueryEventHandler oddEventHandler, params jQueryEventHandler[] eventHandlers) {
