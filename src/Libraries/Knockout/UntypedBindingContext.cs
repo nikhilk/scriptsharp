cd3cc32d433659cdd9c62bd4024512ea5b85c338
Michael Aird <michael.aird@gmail.com>
2012-07-17 15:23:30 -0400

    Added BindingContext to BindingHandler method signatures

---
+++ src/Libraries/Knockout/UntypedBindingContext.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace KnockoutApi
+{
+
+    [Imported]
+    [IgnoreNamespace]
+    public class UntypedBindingContext
+    {
+
+        [IntrinsicProperty]
+        [ScriptName("$data")]
+        public object Data
+        {
+            get;
+            set;
+        }
+
+        [IntrinsicProperty]
+        [ScriptName("$parent")]
+        public object Parent
+        {
+            get;
+            set;
+        }
+
+        [IntrinsicProperty]
+        [ScriptName("$parents")]
+        public object[] Parents
+        {
+            get;
+            set;
+        }
+
+        [IntrinsicProperty]
+        [ScriptName("$root")]
+        public object[] Root
+        {
+            get;
+            set;
+        }
+
+        [IntrinsicProperty]
+        [ScriptName("$index")]
+        public Observable<int> Index
+        {
+            get;
+            set;
+        }
+    }
+}
