5e75f93769a0ab57c508109ee6ed50d74af6ff74
Nikhil Kothari <nikhilk@live.com>
2012-07-20 16:02:16 -0700

    Trim out manual setup/cleanup scripts

--- bin/cleanup.bat
+++ bin/cleanup.bat
@@ -3,25 +3,11 @@
 REM This script cleans up what setup.bat does.
 REM It must be run within an Administrator command prompt.
 
-@echo Cleaning GAC entries...
-gacutil /nologo /uf ScriptSharp
-gacutil /nologo /uf ScriptSharp.Build
-gacutil /nologo /uf ScriptSharp.Testing
-gacutil /nologo /uf ScriptSharp.VisualStudio
-
-set InstallDir=%ProgramFiles(x86)%
-if "%InstallDir%" == "" set InstallDir=%ProgramFiles%
-
-@echo Removing script# folder...
-rd /s /q "%InstallDir%\ScriptSharp\v1.0"
-
-if "%1" neq "includeTemplates" goto Done
-
-:Templates
 @echo Cleaning up templates...
+
 set IDEDir="%VSINSTALLDIR%"\Common7\IDE
+
 rd /s /q "%IDEDir%"\ProjectTemplates\CSharp\Script#
 rd /s /q "%IDEDir%"\ItemTemplates\CSharp\Script#
-%IDEDir%\devenv.exe /installvstemplates
 
-:Done
+%IDEDir%\devenv.exe /installvstemplates
