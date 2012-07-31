ed5af6b1a0d85e06d6f4c67776a44fb35b39c678
Nikhil Kothari <nikhilk@live.com>
2012-07-30 23:01:42 -0700

    Code cleanup

--- src/Core/FileInputStreamSource.cs
+++ src/Core/FileInputStreamSource.cs
@@ -1,5 +1,5 @@
 // FileInputStreamSource.cs
-// Script#/Common
+// Script#/Core
 // This source code is subject to terms and conditions of the Apache License, Version 2.0.
 //
 
--- src/Core/ResourceFile.cs
+++ src/Core/ResourceFile.cs
@@ -1,5 +1,5 @@
 // ResourceFile.cs
-// Script#/Common
+// Script#/Core
 // This source code is subject to terms and conditions of the Apache License, Version 2.0.
 //
 
