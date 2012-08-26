efe7bc7dc8a94743940f36c4474f0603b7c4df5a
nikhilk <nikhilk@live.com>
2012-08-26 11:10:34 -0700

    Fix line endings

--- src/Libraries/Knockout/Observable.cs
+++ src/Libraries/Knockout/Observable.cs
@@ -53,7 +53,7 @@ namespace KnockoutApi {
 
         /// <summary>
         /// For Primitive Types ko will handle Equality internally
-        /// For complex types a supplied function can be assigned to improve 
+        /// For complex types a supplied function can be assigned to improve
         /// change (mutation) detection
         /// </summary>
         [IntrinsicProperty]
