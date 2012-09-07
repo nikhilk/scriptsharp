bd5cc9d967496a38afa7ac843d70268b9df18eaf
nikhilk <nikhilk@live.com>
2012-09-07 15:42:31 -0700

    Fix some broken tests

--- src/Core/CoreScript/BCL/Debug.js
+++ src/Core/CoreScript/BCL/Debug.js
@@ -10,7 +10,7 @@ if (!global.console) {
   }
 }
 
-global.console.fail = function(message) {
+console.fail = function(message) {
   console.assert(false, message);
   if (global.navigator && (global.navigator.userAgent.indexOf('MSIE') > 0)) {
     eval('debugger;');
