8dff04f76229e170f6545424ffe3db76f26b70ae
nikhilk <nikhilk@live.com>
2012-11-04 12:15:39 -0800

    Knockout mapping bindings to support arrays (via @elkdanger)

---
+++ src/Libraries/Knockout/KnockoutMappingArrayCreateOptions.cs
@@ -0,0 +1,37 @@
+﻿// KnockoutMappingArrayCreateOptions.cs
+// Script#/Libraries/Knockout
+// This source code is subject to terms and conditions of the Apache License, Version 2.0.
+//
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace KnockoutApi {
+
+    [IgnoreNamespace]
+    [Imported]
+    [ScriptName("Object")]
+    public sealed class KnockoutMappingArrayCreateOptions {
+
+        [IntrinsicProperty]
+        public object Data {
+            get {
+                return null;
+            }
+        }
+
+        [IntrinsicProperty]
+        public object Parent {
+            get {
+                return null;
+            }
+        }
+
+        [IntrinsicProperty]
+        public object Skip {
+            get {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/Libraries/Knockout/KnockoutMappingArraySpecification.cs
@@ -0,0 +1,43 @@
+﻿// KnockoutMappingArraySpecification.cs
+// Script#/Libraries/Knockout
+// This source code is subject to terms and conditions of the Apache License, Version 2.0.
+//
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace KnockoutApi {
+
+    [IgnoreNamespace]
+    [Imported]
+    [ScriptName("Object")]
+    public abstract class KnockoutMappingArraySpecification {
+
+        internal KnockoutMappingArraySpecification() {
+        }
+    }
+
+    [IgnoreNamespace]
+    [Imported]
+    [ScriptName("Object")]
+    public sealed class KnockoutMappingArraySpecification<TModel> : KnockoutMappingArraySpecification {
+
+        [IntrinsicProperty]
+        public Func<KnockoutMappingArrayCreateOptions, TModel> Create {
+            get {
+                return null;
+            }
+            set {
+            }
+        }
+
+        [IntrinsicProperty]
+        public Func<TModel, object> Key {
+            get {
+                return null;
+            }
+            set {
+            }
+        }
+    }
+}
