8cecb4659cdf1d5c3d29aae52bf6845a0dd44916
MGSOPS\fredp <fred.perkins@derivco.co.uk>
2017-08-22 09:59:28 +0100

    - Fixing naming of input element and merging in the HtmlInputElement into the base InputElement

--- src/Libraries/Web/Html/Element.cs
+++ src/Libraries/Web/Html/Element.cs
@@ -11,7 +11,7 @@ namespace System.Html {
 
     [ScriptIgnoreNamespace]
     [ScriptImport]
-    [ScriptName("Object")]
+    [ScriptName(PreserveCase = true)]
     public class Element {
 
         protected internal Element() {
