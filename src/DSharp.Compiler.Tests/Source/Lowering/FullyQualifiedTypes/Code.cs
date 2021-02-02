b05b5dc7ea102e91abb7448b99450a0cefad8b65
Kevin Reed <kevin.reed@derivco.co.uk>
2021-02-02 10:08:22 +0000

    Feature/lower qualified names (#283)
    * simple implementation for fully qualified names
    
    * using alias for non-generic types

---
+++ src/DSharp.Compiler.Tests/Source/Lowering/FullyQualifiedTypes/Code.cs
@@ -0,0 +1,37 @@
+﻿using System;
+#if X
+using OtherNamespace;
+#endif
+
+[assembly: ScriptAssembly("test")]
+
+namespace Some.Namespace
+{
+    public static class Bar
+    {
+        public Bar(Bar bar)
+        {
+
+        }
+    }
+
+    public static class Bar<T>
+    {
+        public Bar(T t)
+        {
+
+        }
+    }
+}
+
+namespace FullyQualifiedTypesTests
+{
+    public class Foo
+    {
+        public void Main()
+        {
+            Some.Namespace.Bar bar = new Some.Namespace.Bar(new Some.Namespace.Bar());
+            Some.Namespace.Bar<Some.Namespace.Bar> bart = new Some.Namespace.Bar<Some.Namespace.Bar>(new Some.Namespace.Bar());
+        }
+    }
+}
---
+++ src/DSharp.Compiler/Preprocessing/Lowering/FullyQualifiedTypeNameRewriter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace DSharp.Compiler.Preprocessing.Lowering
+{
+    public class FullyQualifiedTypeNameRewriter : CSharpSyntaxRewriter, ILowerer
+    {
+        private SemanticModel sem;
+        private HashSet<string> requiredUsings;
+        private Dictionary<string, string> typeAliases;
+
+        public CompilationUnitSyntax Apply(Compilation compilation, CompilationUnitSyntax root)
+        {
+            sem = compilation.GetSemanticModel(root.SyntaxTree);
+            requiredUsings = new HashSet<string>();
+            typeAliases = new Dictionary<string, string>();
+
+            var newRoot = Visit(root) as CompilationUnitSyntax;
+
+            var missingUsings = requiredUsings.Where(r => !string.IsNullOrWhiteSpace(r)).Where(r => !root.Usings.Select(u => u.Name.ToString()).Contains(r));
+
+            if (missingUsings.Any())
+            {
+                var missingDirectives = missingUsings.Select(s => UsingDirective(ParseName(s).WithLeadingTrivia(Whitespace(" "))).WithTrailingTrivia(CarriageReturn)).ToArray();
+                newRoot = newRoot.AddUsings(missingDirectives);
+            }
+            if (typeAliases.Any())
+            {
+                newRoot = newRoot.AddUsings(CreateTypeAliases());
+            }
+
+            return newRoot;
+        }
+
+        private UsingDirectiveSyntax[] CreateTypeAliases()
+        {
+            bool isFirstUsing = true;
+
+            return typeAliases.Select(s =>
+            {
+                var line = UsingDirective(
+                    NameEquals(s.Key).WithLeadingTrivia(Whitespace(" ")),
+                    ParseName(s.Value)
+                ).WithTrailingTrivia(CarriageReturn);
+
+                if (isFirstUsing)
+                {
+                    isFirstUsing = false;
+                    line = line.WithLeadingTrivia(line.GetTrailingTrivia());
+                }
+
+                return line;
+
+            }).ToArray();
+        }
+
+        public override SyntaxNode VisitQualifiedName(QualifiedNameSyntax node)
+        {
+            var type = sem.GetTypeInfo(node).Type 
+                ?? sem.GetTypeInfo(node.Parent).Type;
+
+            if(type is INamedTypeSymbol namedType && !node.Parent.IsKind(SyntaxKind.UsingDirective))
+            {
+                if(namedType.IsGenericType)
+                {
+                    requiredUsings.Add(type.ContainingNamespace.ToString());
+                    return Visit(node.Right)
+                        .WithTriviaFrom(node);
+                }
+
+                var typeAlias = node.ToString().Replace(".", "_");
+                typeAliases[typeAlias] = node.ToString();
+                return IdentifierName(typeAlias)
+                    .WithTriviaFrom(node);
+            }
+
+            return node;
+        }
+    }
+}
